8b105dea83158a1a4053a6c4f013e6804df5a479
Phillip Chadwick <phillipchadwick@hotmail.com>
2021-09-11 12:23:32 -0700

    adds code for inheritance assignment 1

---
+++ Assignments/Assignment1/Inheritance.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Assignment1
+{
+    class Address
+    {
+        string street;
+        string city;
+        string state;
+        string zipCode;
+
+        public Address(string _street, string _city, string _state, string _zipcode)
+        {
+            street = _street;
+            city = _city;
+            state = _state;
+            zipCode = _zipcode;
+        }
+    }
+
+    class Person
+    {
+        string fName;
+        string lName;
+        string email;
+        string phone;
+        Address address;
+
+        public Person (string _fName, string _lName, string _email, string _phone, Address _address)
+        {
+            fName = _fName;
+            lName = _lName;
+            email = _email;
+            phone = _phone;
+            address = _address;
+        }
+    }
+
+    class Student : Person
+    {
+        string studentID;
+
+        public Student(string _fName, string _lName, string _email, string _phone, Address _address, string _studentID) : base(_fName, _lName, _email, _phone, _address)
+        {
+            studentID = _studentID;
+        }
+    }
+
+    class Instructor : Employee
+    {
+        string[] teaching;
+
+        public Instructor(string _fName, string _lName, string _email, string _phone, Address _address, string _employeeID, string[] _teaching) : base(_fName, _lName, _email, _phone, _address, _employeeID)
+        {
+            teaching = _teaching;
+        }
+    }
+
+    class Administrator : Employee
+    {
+        string officeHours;
+        public Administrator(string _fName, string _lName, string _email, string _phone, Address _address, string _employeeID, string _officeHours) : base(_fName, _lName, _email, _phone, _address, _employeeID)
+        {
+            officeHours = _officeHours;
+        }
+    }
+
+    class Employee : Person
+    {
+        string employeeID;
+
+        public Employee(string _fName, string _lName, string _email, string _phone, Address _address, string _employeeID) : base(_fName, _lName, _email, _phone, _address)
+        {
+            employeeID = _employeeID;
+        }
+    }
+}
--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Assignments.Assignment1;
 
 namespace Assignments
 {
@@ -6,7 +7,16 @@ namespace Assignments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World bb!");
+
+            string[] classes = new string[] {"math", "science"};
+
+            Administrator admin = new Administrator("jim", "jam", "admin@email.com", "(123)456-7445", new Address("222nd", "vancouver", "BC", "v2x-555"), "123admin", "9am-5pm");
+            Instructor instructor = new Instructor("jane", "john", "instruct@email.com", "(123)456-1234", new Address("222nd", "vancouver", "BC", "v2x-555"), "123teach", classes);
+            Student student = new Student("Tom", "Tron", "student@email.com", "(123)456-6534", new Address("222nd", "vancouver", "BC", "v2x-555"), "123student");
+
+            
+
         }
     }
 }
