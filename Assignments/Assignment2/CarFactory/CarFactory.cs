ee7d353733676385ade4cfe9cae96581813acaf1
Phillip Chadwick <phillipchadwick@hotmail.com>
2021-10-28 22:36:07 -0700

    refactors and abstracts carFactory, updates install instructions for xml configs

--- Assignments/Assignment2/CarFactory/CarFactory.cs
+++ Assignments/Assignment2/CarFactory/CarFactory.cs
@@ -14,76 +14,62 @@ public class CarFactory
         this.logger = logger;
     }
 
-    public CarV2 buildElectricCar(int configNumber, CarBuilder builder)
+    public CarV2 buildElectricCar(CarBuilder builder)
     {
-        var car = builder.addBrand(CarConfigs.Instance.getConfigs()[configNumber].brand)
-            .addmilePer(CarConfigs.Instance.getConfigs()[configNumber].mileper)
-            .addmodel(CarConfigs.Instance.getConfigs()[configNumber].model)
-            .addvinNumber(CarConfigs.Instance.getConfigs()[configNumber].vin)
-            .addyearBuilt(CarConfigs.Instance.getConfigs()[configNumber].yearBuilt)
-            .build(CarBuilder.carTypes.Electric);
+        var car = builder.build(CarBuilder.carTypes.Electric);
 
         return car;
     }
 
-    public CarV2 buildGasCar(int configNumber, CarBuilder builder)
+    public CarV2 buildGasCar(CarBuilder builder)
     {
-        var car = builder.addBrand(CarConfigs.Instance.getConfigs()[configNumber].brand)
-            .addmilePer(CarConfigs.Instance.getConfigs()[configNumber].mileper)
-            .addmodel(CarConfigs.Instance.getConfigs()[configNumber].model)
-            .addvinNumber(CarConfigs.Instance.getConfigs()[configNumber].vin)
-            .addyearBuilt(CarConfigs.Instance.getConfigs()[configNumber].yearBuilt)
-            .build(CarBuilder.carTypes.Gas);
+        var car = builder.build(CarBuilder.carTypes.Gas);
 
         return car;
     }
 
-    public CarV2 buildHybridCar(int configNumber, CarBuilder builder)
+    public CarV2 buildHybridCar(CarBuilder builder)
     {
-        var car = builder.addBrand(CarConfigs.Instance.getConfigs()[configNumber].brand)
-            .addmilePer(CarConfigs.Instance.getConfigs()[configNumber].mileper)
-            .addmodel(CarConfigs.Instance.getConfigs()[configNumber].model)
-            .addvinNumber(CarConfigs.Instance.getConfigs()[configNumber].vin)
-            .addyearBuilt(CarConfigs.Instance.getConfigs()[configNumber].yearBuilt)
-            .build(CarBuilder.carTypes.Hybrid);
+        var car = builder.build(CarBuilder.carTypes.Hybrid);
 
         return car;
     }
 
-    public CarV2 buildTimeMachine(int configNumber, CarBuilder builder)
+    public CarV2 buildTimeMachine(CarBuilder builder)
     {
-        var car = builder.addBrand(CarConfigs.Instance.getConfigs()[configNumber].brand)
-                        .addmilePer(CarConfigs.Instance.getConfigs()[configNumber].mileper)
-                        .addmodel(CarConfigs.Instance.getConfigs()[configNumber].model)
-                        .addvinNumber(CarConfigs.Instance.getConfigs()[configNumber].vin)
-                        .addyearBuilt(CarConfigs.Instance.getConfigs()[configNumber].yearBuilt)
-                        .build(CarBuilder.carTypes.TimeMachine);
+        var car = builder.build(CarBuilder.carTypes.TimeMachine);
 
         return car;
     }
 
     public CarV2 BuildCarsFromConfig(int configNumber)
     {
-        var builder = new CarBuilder(logger);
         CarV2 car;
+
+        var builder = new CarBuilder(logger);
+        builder.addBrand(CarConfigs.Instance.getConfigs()[configNumber].brand)
+            .addmilePer(CarConfigs.Instance.getConfigs()[configNumber].mileper)
+            .addmodel(CarConfigs.Instance.getConfigs()[configNumber].model)
+            .addvinNumber(CarConfigs.Instance.getConfigs()[configNumber].vin)
+            .addyearBuilt(CarConfigs.Instance.getConfigs()[configNumber].yearBuilt);
 
         switch (CarConfigs.Instance.getConfigs()[configNumber].carType)
         {
             case (CarBuilder.carTypes.Electric):
             
-                car = buildElectricCar(configNumber, builder);
+                car = buildElectricCar(builder);
                 break;
             
             case (CarBuilder.carTypes.Gas):
-                car = buildGasCar(configNumber, builder);
+                car = buildGasCar(builder);
                 break;
 
             case (CarBuilder.carTypes.Hybrid):
-                car = buildHybridCar(configNumber, builder);
+                car = buildHybridCar(builder);
                 break;
 
             case (CarBuilder.carTypes.TimeMachine):
-                car = buildTimeMachine(configNumber, builder);
+                car = buildTimeMachine(builder);
                 break;
             default:
                 throw new ArgumentException("no valid cartype given on factory build.");
